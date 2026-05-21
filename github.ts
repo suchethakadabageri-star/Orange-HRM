658950b6fc4b3b7e8e38875312a0a71c19a1792e
Suchetha Kadabageri <Suchetha.Kadabageri@talentica.com>
2026-05-21 16:20:47 +0530

    Remove all code files

--- github.ts
+++
@@ -1,8 +0,0 @@
-//GitHub/Gitlab/Bitbucket - version control tools
-//To work with GitHub/Gitlab/Bitbucket, we need to have 'git' installed on our local machine.
-//we can download git from https://git-scm.com/downloads
-
-//1. You are the first person to start and there is no repo aavialbel. Create a new repository on the version control tool
-//2. create a project and wright testcases for the project.
-//3. git init - to initialize a new git repository.
-//4. git add . - to add all the files in your local git area.
